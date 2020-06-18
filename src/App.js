8972ecc3d6d0927a0ff604a28d88f456556b1576
timeonator <timeonator@gmail.com>
2020-06-18 00:26:04 -0700

    some minor cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import ChartWrapper from './D3ChartWrapper'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <ChartWrapper />
     </div>
   );
 }
